2e46ce06589283be72dbcae8c1a1d0dea8c98cd0
GoldenChicken <paveltikhomirov41@gmail.com>
2018-09-07 12:18:42 -0700

    hybrhtc

--- schoolppp/school/models.py
+++ schoolppp/school/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Schools(models.Model):
+    name=models.CharField(max_length=100)
+    address=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+    class Meta:
+        db_table="schools"
+class SchoolData(models.Model):
+    school_id=models.IntegerField(max_length=100)
+    ele_k=models.IntegerField()
+    ele_u=models.IntegerField()
+    hea_k=models.IntegerField()
+    hea_u=models.IntegerField()
+    wat_l=models.IntegerField()
+    wat_u=models.IntegerField()
+    class Meta:
+        db_table="schooldata"
+    
+    
--- schoolppp/school/views.py
+++ schoolppp/school/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
+from .models import SchoolData, School
+def home(request):
+    return render(request,'index.html')
+def school_list(request):
+    schools=School.objects.all()
+    return render(request,'schooldata_list.html', {'school':})
